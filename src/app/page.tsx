1a8dfee545d4fdcbd859932af75adf21b3f98a08
Abdul Sami <abdulsamisiddiq2001@gmail.com>
2024-09-24 01:31:18 +0500

    nextj and shadcn ui initialization and configuration

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <h2>Nextjs Project Initialization</h2>
+      <h2>Shadcn UI Configuration</h2>
+    </div>
+  );
+}
