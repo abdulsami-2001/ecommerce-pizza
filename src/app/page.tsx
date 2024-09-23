1c3780dac70c38e62bbd553d05582a4505b6a2eb
Abdul Sami <abdulsamisiddiq2001@gmail.com>
2024-09-24 01:46:48 +0500

    add custom theme colors in ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
       <h1>Hello World</h1>
       <h2>Nextjs Project Initialization</h2>
       <h2>Shadcn UI Configuration</h2>
+      <Button variant={"outline"}>Hello</Button>
     </div>
   );
 }
